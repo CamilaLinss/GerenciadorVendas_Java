2cf01c69363bf42fd82c5e6bea9ddc226ca69a35
CamilaLinss <souzacamila215@ymail.com>
2021-02-17 18:59:40 -0300

    Ajustes

--- Gerenciador_Vendas/src/BancoDados/BancoDados.java
+++ Gerenciador_Vendas/src/BancoDados/BancoDados.java
@@ -174,50 +174,6 @@ public void desconectar() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-ResultSet Busca_Funcionario(String tabela, String campo1,  String campo2, String campo3, 
-		String campo4, String campo5,String dado1,String dado2,String dado3,String dado4,String dado5) {
-
-	try {
-		String query = "SELECT * FROM "+tabela+" WHERE "+campo1+" LIKE '%" + dado1 + "%' AND "+campo2+" LIKE '%" + dado2 +"%' AND "+campo3+" LIKE '%" + dado3 + "%'"
-				+ " AND "+ campo4 +" LIKE '%" + dado4 + "%' AND "+campo5+" LIKE '%" + dado5 + "%';";
-		
-		JOptionPane.showMessageDialog(null,query);
-		//executeQuery usado apenas para SELECT
-		this.resultado = this.consulta.executeQuery(query);
-		
-		this.resultado.next();
-		
-	}
-		catch(Exception fa) {
-			//System.out.println("ERRO: " + fa.getMessage());
-			JOptionPane.showMessageDialog(null, "ERRO NA BUSCA: " + fa);	
-		}
-
-	
-	
-	return resultado;
-
-}
-	
-
-
-
-	
 	
 	
 }
