9e376b1e5240a61af2026fb6939a0572b5204d0a
CamilaLinss <souzacamila215@ymail.com>
2021-03-03 21:46:32 -0300

    Teste coneção Junit5

---
+++ Gerenciador_Vendas/src/Testes/ConexaoTeste.java
@@ -0,0 +1,18 @@
+package Testes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ConexaoTeste {
+
+	@Test
+	void testConexao_BD() {
+		
+		
+		
+		
+		
+	}
+
+}
